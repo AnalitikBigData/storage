dd0f3a8129bd7d95ddf6889a69184b1db3e06248
Veronika <morozznika@gmail.com>
2022-08-23 11:31:49 +0300

    localStorage

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+const toDoData = [];
+
+let copyToDoData = [];
+
+const rendre = function(){
+    todoList.innerHTML = '';
+    todoCompleted.innerHTML = '';
+    toDoData.forEach(function(item, index){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML = '<span class="text-todo">' + item.text +'</span>' + 
+            '<div class="todo-buttons">'+
+		    '<button class="todo-remove">' + '</button>' + 
+		    '<button class="todo-complete"' + '</button>' + 
+		    '</div>';
+
+        if(item.completed === true){
+            todoCompleted.append(li);
+            localStorage.setItem('toDoData', JSON.stringify(toDoData));
+        }
+        else {
+            todoList.append(li);
+            localStorage.setItem('toDoData', JSON.stringify(toDoData));
+        }
+
+        li.querySelector('.todo-complete').addEventListener('click', function(){
+            item.completed = !item.completed;
+            rendre();
+            localStorage.setItem('toDoData', JSON.stringify(toDoData));
+        })
+        li.querySelector('.todo-remove').addEventListener('click', function(){
+            toDoData.splice(index, 1);
+
+            rendre();
+            localStorage.setItem('toDoData', JSON.stringify(toDoData));
+        })
+    })
+}
+
+todoControl.addEventListener('submit', function(event){
+    event.preventDefault();
+    const newToDo = {
+        text: headerInput.value,
+        completed: false,
+    }
+    if(newToDo.text.trim() !== ''){
+        toDoData.push(newToDo);
+    }
+    headerInput.value = '';
+    rendre();
+});
+copyToDoData = JSON.parse(localStorage.getItem('toDoData'));
+for(let i = 0; i < copyToDoData.length; i++){
+    toDoData.push(copyToDoData[i]);
+}
+
+//localStorage.clear();
